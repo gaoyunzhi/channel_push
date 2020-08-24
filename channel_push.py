12dcda11bddc475dcef4d31ecac9b1a47d8952ff
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-24 12:30:01 -0700

    commit

--- channel_push.py
+++ channel_push.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from telegram.ext import Updater, MessageHandler, Filters
-from telegram_util import log_on_fail
+from telegram_util import log_on_fail, splitCommand, matchKey
 import random
 import threading
 import plain_db
-
-channels = plain_db.loadNoKeyDB('existing')
 
 with open('credential') as f:
 	token = f.read().strip()
@@ -15,35 +13,39 @@ with open('credential') as f:
 tele = Updater(token, use_context=True) # @channel_push_bot
 debug_group = tele.bot.get_chat(420074357)
 channel_push = tele.bot.get_chat('@channel_push')
-
+channels = plain_db.loadNoKeyDB('existing')
 
 def findChannels(text):
     result = []
     for x in text.split():
-            if not x:
-                    continue
-            if x.startswith('@'):
-                    result.append(Channel('https://t.me/' + x[1:]))
-                    continue
-            if 't.me' in x:
-                    result.append(Channel(x))
-                    continue
-    result = [x for x in result if x.exist()]
-    [x.save(db) for x in result]
-    return result
+        if not x:
+            continue
+        if x.startswith('@'):
+            channels.add('https://t.me/' + x[1:])
+        if 't.me' in x:
 
 @log_on_fail(debug_group)
 def handlePrivate(update, context):
 	msg = update.effective_message
-	if not msg.text:
-		msg.reply_text(HELP_MESSAGE)
+	if msg.chat.id != debug_group.id:
 		return
-	channels = findChannels(msg.text)
-	if not channels:
-		msg.reply_text('no channel/group found.\n' + HELP_MESSAGE)
+	command, text = splitCommand(msg.text)
+	if matchKey(command, ['remove']):
+		channels.remove(text)
+		msg.reply('Removed ' + text)
 		return
-	channel_list = [x.getLink() for x in channels]
-	msg.reply_text('Channels/groups recorded:\n' + '\n'.join(channel_list))
+	count = 0
+	for piece in msg.text.split():
+        if not piece:
+            continue
+        if piece.startswith('@'):
+        	count += 1
+            channels.add('https://t.me/' + x[1:])
+            continue
+        if 't.me' in piece:
+        	count += 1
+        	channels.add(piece)
+    msg.reply('Added %s items' % count)
 
 @log_on_fail(debug_group)
 def sendPush():
